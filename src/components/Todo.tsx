dc435bf115d7bdc4ea02f10e708fb05f5d1bcda9
jenipharachel <jenipharachel@gmail.com>
2023-11-24 15:38:55 +0530

    Updated design

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -4,9 +4,10 @@ import {StyleSheet, Text, View} from 'react-native';
 export const Todo = ({todo}) => {
   return (
     <View style={styles.container} key={todo.id}>
-      <Text>
+      <Text style={{flex: 3}}>
         {todo.id}. {todo.title}
       </Text>
+      <Text>{todo.completed ? 'Completed' : 'In progress'}</Text>
     </View>
   );
 };
@@ -17,5 +18,10 @@ const styles = StyleSheet.create({
     marginHorizontal: 20,
     marginVertical: 10,
     backgroundColor: 'lightblue',
+    borderRadius: 10,
+    padding: 10,
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
 });
