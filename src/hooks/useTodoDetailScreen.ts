cdbca917d3423cee92784318a75fe86c717c6581
jenipharachel <jenipharachel@gmail.com>
2023-11-28 11:51:50 +0530

    edit view ui

--- src/hooks/useTodoDetailScreen.ts
+++ src/hooks/useTodoDetailScreen.ts
@@ -1,10 +1,20 @@
+import {useState} from 'react';
 import {useGetTodoDetailQuery} from '../apis/todoApi';
 
 export const useTodoDetailScreen = (id: number) => {
   const {data: todoDetail, isLoading} = useGetTodoDetailQuery(id);
+  const [todoTitle, setTodoTitle] = useState(todoDetail?.title || '');
+  const [isCompleted, setIsCompleted] = useState(todoDetail?.completed);
+
+  const onChangeTodoTitle = (text: string) => setTodoTitle(text);
+  const onStatusChange = (bool: boolean) => setIsCompleted(bool);
 
   return {
     todoDetail,
     isLoading,
+    todoTitle,
+    onChangeTodoTitle,
+    isCompleted,
+    onStatusChange,
   };
 };
--- src/screens/TodoDetailScreen.tsx
+++ src/screens/TodoDetailScreen.tsx
@@ -1,42 +1,104 @@
-import React from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  Switch,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import {useTodoDetailScreen} from '../hooks/useTodoDetailScreen';
 
 export const TodoDetailScreen = props => {
+  const [isEditView, setIsEditView] = useState(false);
+
   const {id} = props.route.params;
-  const {todoDetail, isLoading} = useTodoDetailScreen(id);
+  const {
+    todoDetail,
+    isLoading,
+    todoTitle,
+    onChangeTodoTitle,
+    isCompleted,
+    onStatusChange,
+  } = useTodoDetailScreen(id);
 
   console.log(todoDetail, 'todoDetail', isLoading);
 
+  if (isLoading) {
+    return (
+      <View style={[styles.container, styles.center]}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.heading}> Todo Detail Screen</Text>
       <View style={styles.box}>
-        <View>
-          <Text>Todo: {todoDetail.title}</Text>
-          <Text>
-            Status: {todoDetail.completed ? 'Completed' : 'In progress'}
-          </Text>
-        </View>
-        <TouchableOpacity>
-          <Text style={styles.edit}>Edit</Text>
-        </TouchableOpacity>
+        {isEditView ? (
+          <>
+            <View style={styles.leftPane}>
+              <Text>Todo:</Text>
+              <TextInput
+                style={styles.input}
+                onChangeText={onChangeTodoTitle}
+                value={todoTitle}
+              />
+              <Text>Status:</Text>
+              <Switch
+                trackColor={{false: '#767577', true: 'green'}}
+                thumbColor="#f4f3f4"
+                ios_backgroundColor="#3e3e3e"
+                onValueChange={onStatusChange}
+                value={isCompleted}
+              />
+            </View>
+            <TouchableOpacity onPress={() => setIsEditView(false)}>
+              <Text style={styles.edit}>Done</Text>
+            </TouchableOpacity>
+          </>
+        ) : (
+          <>
+            <View>
+              <Text>Todo: {todoDetail?.title}</Text>
+              <Text>
+                Status: {todoDetail?.completed ? 'Completed' : 'In progress'}
+              </Text>
+            </View>
+            <TouchableOpacity onPress={() => setIsEditView(true)}>
+              <Text style={styles.edit}>Edit</Text>
+            </TouchableOpacity>
+          </>
+        )}
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  heading: {fontSize: 20},
+  container: {flex: 1},
+  heading: {fontSize: 20, margin: 10},
   box: {
-    borderWidth: 1,
-    borderColor: 'black',
-    margin: 10,
+    marginHorizontal: 20,
+    marginVertical: 10,
     padding: 10,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    minHeight: 70,
+    backgroundColor: 'lightblue',
+    borderRadius: 10,
   },
   edit: {
     color: 'green',
   },
+  center: {justifyContent: 'center', alignItems: 'center'},
+  input: {
+    height: 40,
+    marginVertical: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  leftPane: {width: '85%'},
 });
